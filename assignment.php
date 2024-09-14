7d6853c0592a0d419b3033d70e57ad791b8440f1
abdullahiheart <abdalaheart@gmail.com>
2024-09-14 12:18:43 +0300

    assignment one

---
+++ assignment.php
@@ -0,0 +1,15 @@
+<?php
+$greeting = "Hello";
+$noun ="World";
+
+echo $greeting . " " . $noun . "! ";
+
+$greeting = "Good by";
+$noun ="abti";
+
+echo $greeting . " " . $noun . "! ";
+
+$greeting = "Well come ";
+$noun ="every one ";
+
+echo $greeting . " " . $noun . "!";
