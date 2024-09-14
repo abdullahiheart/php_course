c7bbe627198d1f2748aeed7ef28b2338d12d5e2e
abdullahiheart <abdalaheart@gmail.com>
2024-09-14 14:53:16 +0300

    local variables

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
-//$abti = "Waxaan ahay Arday";
-//echo $abti;
-#concatinatopns
-$hello = "Waxaan ahay";
-//echo $hello ." "."arday";
-# wado kaloo gaban oo loo isticmali karo
-echo "$hello ardaga";
+//Waxaa jira three variables
+////1: Local variables
+////2: global variables
+////3: Static variables
+//function abdfunction() {
+//    $abti = "waxaa sameyaa systems";
+//    echo $abti;
+//
+//}
+//abdfunction();
