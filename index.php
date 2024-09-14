f9571a4df3c6eb387fe7ee18d1f1034bbc6b56f3
abdullahiheart <abdalaheart@gmail.com>
2024-09-14 12:09:27 +0300

    concatinations

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-$abti = "Waxaan ahay Arday";
-echo $abti;
+//$abti = "Waxaan ahay Arday";
+//echo $abti;
+#concatinatopns
+$hello = "Waxaan ahay";
+//echo $hello ." "."arday";
+# wado kaloo gaban oo loo isticmali karo
+echo "$hello ardaga";
