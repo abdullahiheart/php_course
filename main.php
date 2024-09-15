df6cee5e3df811be0d40dccbc76a987a578b25af
abdullahiheart <abdalaheart@gmail.com>
2024-09-15 09:50:37 +0300

    assignment

---
+++ main.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>System Development</title>
+</head>
+<body>
+  <h1> Soo dhawaaw Sxb</h1>
+  <?php
+  /*$ab =[
+      "waxaa sameyaa systems",
+      "waxaa soo bandhigaa",
+      "waxaa dhisaa mobile app"
+  ];
+  ?>
+<?php foreach ($ab as $a){
+    echo " <li> $a </li>";
+} */
+
+  $arr = [
+      "abdullahi",
+      "Khalid",
+      "Allas"
+  ];
+  foreach ($arr as $value) {
+      echo "<h2> $value </h2>";
+  }
+?>
+
+
+</body>
+</html>
